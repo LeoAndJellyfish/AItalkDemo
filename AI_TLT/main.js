3d5d5c205f17bdff974d0f1b1a4949b341061dcb
LeoAndJellyfish <yyzjmk0123@163.com>
2024-10-24 13:19:14 +0800

    0

--- AI_TLT/main.js
+++
@@ -1,7 +0,0 @@
-import Drawflow from 'drawflow';
-import styleDrawflow from 'drawflow/dist/drawflow.min.css';
-<div id="drawflow"></div>
-var id = document.getElementById("drawflow");
-const editor = new Drawflow(id);
-editor.start();
-styleDrawflow.use();
